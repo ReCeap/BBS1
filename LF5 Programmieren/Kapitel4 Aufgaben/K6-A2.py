3d382fc2d95768eb137acafedd9612a20418dad0
ReCeap <receap@web.de>
2021-09-15 12:06:47 +0200

    15-09_12-06

--- LF5 Programmieren/Kapitel4 Aufgaben/K6-A2.py
+++ LF5 Programmieren/Kapitel4 Aufgaben/K6-A2.py
@@ -3,9 +3,9 @@ if __name__ == "__main__":
     def spritkosten(kilometer, verbrauch, preis_Liter):
         return (((verbrauch/100)*kilometer)*preis_Liter)
 
-    print(spritkosten(30, 6, 1.36))
+    print(round(spritkosten(30, 6, 1.36),2), "€")
 
 """
 Laufende Kosten werden hier nicht mit einberechnet.
-Es fehlen zum Beispiel Versicherungskosten, Verschleiß oder auch Steuern.
+Es fehlen zum Beispiel Versicherungskosten, Wertverlust, Verschleiß oder auch Steuern.
 """
