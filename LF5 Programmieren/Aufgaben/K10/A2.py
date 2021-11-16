8154f87079676453d264387164d1b732b653744c
ReCeap <receap@web.de>
2021-11-16 10:25:41 +0100

    16-11_10-25

--- LF5 Programmieren/Aufgaben/K10/A2.py
+++ LF5 Programmieren/Aufgaben/K10/A2.py
@@ -1,20 +1,26 @@
+list = [1,4,3,2,34,53,564,47,6,37,45,74568,568,5]
+
 def sortieren_newlist(liste):
     returnlist=[]
-    while len(liste) !=0:
-        returnlist.append(min(liste))
-        liste.remove(min(liste))
+    l=liste.copy()
+    while len(l) !=0:
+        returnlist.append(min(l))
+        l.remove(min(l))
     return returnlist
 
-print(sortieren_newlist([1,7,5,54,7,8,9]))
+print(sortieren_newlist(list))
+print(list)
 
 def umsortierung(liste):
-    for i in range(len(liste)):
-        for i in range(len(liste)-1):
-            if liste[i] > liste[i+1]:
-                g=liste[i]
-                k=liste[i+1]
-                liste[i] = k
-                liste[i+1] = g
-    return liste
+    l=liste.copy()
+    for i in range(len(l)):
+        for i in range(len(l)-1):
+            if l[i] > l[i+1]:
+                g=l[i]
+                k=l[i+1]
+                l[i] = k
+                l[i+1] = g
+    return l
 
-print(umsortierung([1,4,3,2,34,53,564,47,6,37,45,74568,568,5]))
+print(umsortierung(list))
+print(list)
