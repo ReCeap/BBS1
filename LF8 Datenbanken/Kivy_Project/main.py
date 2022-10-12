c4f02459ac84155bbfc5763c97379a3176ea7c34
2regul4r <43504650+ReCeap@users.noreply.github.com>
2022-10-12 16:42:30 +0200

    12-10_16-42

---
+++ LF8 Datenbanken/Kivy_Project/main.py
@@ -0,0 +1,14 @@
+import kivy
+from kivy.app import App
+from kivy.uix.label import Label
+from kivy.uix.widget import Widget
+
+class MainWidget(Widget):
+    pass
+
+class ProjektApp(App):
+    def build(self):
+        pass
+    
+if __name__ == "__main__":
+    ProjektApp().run()
